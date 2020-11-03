ccc4ab6fb7053fb56b2d81a379a3b5973007f129
Tabatha Slatton <valbatha@gmail.com>
2020-11-03 14:39:38 -0700

    fixed google auth issues

--- app/controllers/omniauth_callbacks_controller.rb
+++ app/controllers/omniauth_callbacks_controller.rb
@@ -1,22 +1,11 @@
 class OmniauthCallbacksController < Devise::OmniauthCallbacksController
-    before_action :authenticate_user!, except: :google_oauth2
+    # before_action :authenticate_user!, except: :google
     # skip_before_action :authenticate_user!
 
-  def google_oauth2
-    auth = request.env["omniauth.auth"]
-    user = User.where(provider: auth["provider"], uid: auth["uid"]).first_or_initialize(email: auth["info"]["email"])
-        user.username = auth.info.name
-        user.first_name = auth.info.first_name
-        user.last_name = auth.info.last_name
-        user.profile_img_url = auth.info.image
-        user.email = auth.info.email
-        user.save
-
-    user.remember_me = true
-    sign_in(:user, user)
-
-    redirect_to after_sign_in_path_for(user)
-  end
+ def google_oauth2
+    @user = User.google_omniauth(request.env['omniauth.auth'])
+    sign_in_and_redirect @user
+ end
 
 #     def omniauth
 #         @user = User.from_omniauth(auth)
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,9 +3,20 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable, 
-         :omniauthable, omniauth_providers: [:google_oauth2]
+         :omniauthable 
+        #  omniauth_providers: [:google_oauth2]
 
-  
+         def self.google_omniauth(auth)
+          #  binding.pry
+          where(provider: auth["provider"], uid: auth["uid"]).first_or_create(email: auth["info"]["email"]) do |user|
+              user.username = auth.info.name
+              user.first_name = auth.info.first_name
+              user.last_name = auth.info.last_name
+              user.profile_img_url = auth.info.image
+              user.email = auth.info.email
+              user.password = Devise.friendly_token[0,20]
+          end
+        end
   # def self.from_omniauth(auth)
   #   where(email: auth.info.email).first_or_initialize do |user|
   #     user.username = auth.info.name
