0b708060ab3cb530e4a7143b4d3c5f8feaf60230
Tabatha Slatton <valbatha@gmail.com>
2020-11-04 09:01:32 -0700

    added profile picture logic

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -7,6 +7,7 @@ class RegistrationsController < Devise::RegistrationsController
     end
     
     def account_update_params
+        # binding.pry
         params.require(:user).permit(:email, :password, :password_confirmation, :current_password, :first_name, :last_name, :username, :profile_img_url, :starting_weight, :current_weight, :long_term_goal, :goal_weight)
     end
 
