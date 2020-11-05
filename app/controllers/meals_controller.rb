719198db5684376e7be5d2004493bee560ddd736
Tabatha Slatton <valbatha@gmail.com>
2020-11-05 13:19:34 -0700

    starting working on meal adding functionality

--- app/controllers/meals_controller.rb
+++ app/controllers/meals_controller.rb
@@ -1,2 +1,33 @@
 class MealsController < ApplicationController
+    before_action :set_meal, only: [:show, :edit, :update, :destroy]
+
+    def new
+        @daily_journal = DailyJournal.new
+        @new_meal = @daily_journal.meals.build
+        @food_1 = @new_meal.food_items.build
+        @food_2 = @new_meal.food_items.build
+        @food_3 = @new_meal.food_items.build
+
+        #build out nested object
+            # @daily_journal.workouts.build()
+    end
+
+    def create
+        @daily_journal = DailyJournal.new(daily_journal_params)
+        if @daily_journal.save
+            redirect_to @daily_journal
+        else
+            render :new
+        end
+    end
+  
+    
+    private
+    def set_meal
+        @meal = Meal.find_by_id(params[:id])
+    end
+
+    def daily_journal_params
+        params.require(:meal).permit(:daily_journal_id, :time_eaten, :notes, :category, :food_items[:name, :calories, :carbs, :protein, :fat])
+    end
 end
--- app/models/meal.rb
+++ app/models/meal.rb
@@ -1,4 +1,6 @@
 class Meal < ApplicationRecord
     belongs_to :daily_journal
     has_many :food_items
+
+    accepts_nested_attributes_for :food_items, reject_if: proc { |attributes| attributes['name'].blank? ||  attributes['calories'].blank?}
 end
