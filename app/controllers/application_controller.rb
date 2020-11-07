b6bd5a55543320bfaafb514db7649ef0fe868b2a
Tabatha Slatton <valbatha@gmail.com>
2020-11-07 12:34:03 -0700

    cleaned up and styled meals form

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,13 +5,4 @@ class ApplicationController < ActionController::Base
        @user = current_user 
     end
 
-# protected
-#   def authenticate_user!
-#     if user_signed_in?
-#       super
-#     else
-#       redirect_to new_user_session_path, :alert => 'Must be logged in to view this page'
-#     end
-#   end
-
 end
