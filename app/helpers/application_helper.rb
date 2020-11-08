cc7c7c82e782256998d023cdc0d0035f39732820
Tabatha Slatton <valbatha@gmail.com>
2020-11-08 12:51:19 -0700

    added pounds lost to home page

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,4 +4,12 @@ module ApplicationHelper
         Time.now.strftime('%A, %b %d')
     end
 
+    def pounds_lost(user)
+        user.starting_weight-user.current_weight
+    end
+
+    def pounds_to_go(user)
+        user.current_weight-user.goal_weight
+    end
+
 end
